40872f56f3e7f1ff5278b60e44695dd25882f829
Diegoaph <Diegoaph2018@gmail.com>
2023-08-23 19:01:20 -0500

    copycat

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
-import { getAllVideogames, updateRenderArray } from "../../redux/actions";
 import Nav from "../nav/Nav";
 import Card from "../card/Card";
 import style from "./cards.module.css";
+
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { getAllVideogames, updateRenderArray } from "../../redux/actions";
 
 const Cards = () => {
     const dispatch = useDispatch();
@@ -12,31 +13,27 @@ const Cards = () => {
     const [selectedGenre, setSelectedGenre] = useState("");
     const [selectedSource, setSelectedSource] = useState("");
     const [selectedSort, setSelectedSort] = useState("");
-    const allVideogamesArray = useSelector((state) => state.allVideogamesArray);
-    const itemsPerPage = 15;
-    const totalPages = Math.ceil(allVideogamesArray.length / itemsPerPage);
+    const toRenderArray = useSelector((state) => state.toRenderArray);
 
-    useEffect(() => {
-        dispatch(getAllVideogames());
-    }, [dispatch]);
+    const allVideogamesArray = useSelector((state) => state.allVideogamesArray);
+
+    const [pages, setPages] = useState(
+        Math.ceil(allVideogamesArray.length / 15)
+    );
 
     const onSearch = async (name) => {
         try {
-            const response = await axios.get(
-                `/videogames/search/?name=${name}`
-            );
+            const response = await axios(`videogames/search/?name=${name}`);
             const data = response.data;
 
             if (!data.length) {
-                window.alert("¡Prueba otro nombre!");
+                window.alert("Try another name!");
             } else {
                 dispatch(updateRenderArray(data));
             }
         } catch (error) {
-            console.error(
-                "Error al obtener los resultados de búsqueda:",
-                error
-            );
+            console.error("Error fetching search results:", error);
+            window.alert("no videgames found");
         }
     };
 
@@ -53,52 +50,44 @@ const Cards = () => {
     };
 
     const handleLast = () => {
-        setPage(totalPages);
+        setPage(pages);
     };
 
     const handleNext = () => {
-        setPage((prevPage) => Math.min(prevPage + 1, totalPages));
+        setPage(page + 1);
     };
 
     const handlePrev = () => {
-        setPage((prevPage) => Math.max(prevPage - 1, 1));
+        setPage(page - 1);
     };
 
     const handleFirst = () => {
         setPage(1);
     };
 
-    let filteredGames = allVideogamesArray.slice();
+    let midRender = toRenderArray || allVideogamesArray;
 
     if (selectedGenre !== "all" && selectedGenre !== "") {
-        filteredGames = filteredGames.filter((game) =>
-            game.genres.some((genre) => genre.name === selectedGenre)
-        );
+        midRender = midRender.filter((game) => {
+            return game.genres.some((genre) => genre.name === selectedGenre);
+        });
     }
 
     switch (selectedSort) {
         case "RatingD":
-            filteredGames = filteredGames
-                .slice()
-                .sort((a, b) => b.rating - a.rating);
+            midRender = midRender.sort((a, b) => b.rating - a.rating);
             break;
         case "RatingA":
-            filteredGames = filteredGames
-                .slice()
-                .sort((a, b) => a.rating - b.rating);
+            midRender = midRender.sort((a, b) => a.rating - b.rating);
             break;
         case "AlfaA":
-            filteredGames = filteredGames
-                .slice()
-                .sort((a, b) => b.name.localeCompare(a.name));
+            midRender = midRender.sort((a, b) => b.name.localeCompare(a.name));
             break;
         case "AlfaD":
-            filteredGames = filteredGames
-                .slice()
-                .sort((a, b) => a.name.localeCompare(b.name));
+            midRender = midRender.sort((a, b) => a.name.localeCompare(b.name));
             break;
         case "default":
-            filteredGames = allVideogamesArray.slice();
+            midRender = allVideogamesArray;
             break;
         default:
             break;
@@ -106,31 +95,23 @@ const Cards = () => {
 
     switch (selectedSource) {
         case "DB":
-            filteredGames = filteredGames.filter(
-                (game) => typeof game.id === "string"
-            );
+            midRender = midRender.filter((game) => typeof game.id == "string");
             break;
         case "API":
-            filteredGames = filteredGames.filter(
-                (game) => typeof game.id === "number"
-            );
+            midRender = midRender.filter((game) => typeof game.id == "number");
             break;
         case "all":
-            filteredGames = allVideogamesArray.slice();
+            midRender = allVideogamesArray;
             break;
         default:
             break;
     }
 
-    const finalRender = filteredGames.slice(
-        (page - 1) * itemsPerPage,
-        page * itemsPerPage
-    );
+    let finalRender = midRender.slice(page * 15 - 15, page * 15);
 
     useEffect(() => {
-        setPage(1); // Reset page to 1 when changing filters/sorting
-    }, [selectedGenre, selectedSort, selectedSource]);
-
+        setPages(Math.ceil(allVideogamesArray.length / 15));
+    }, [finalRender, allVideogamesArray.length]);
     return (
         <main className={style.main}>
             <Nav
@@ -141,50 +122,58 @@ const Cards = () => {
             />
 
             <section className={style.cards}>
-                {finalRender.map(
-                    ({ id, name, genres, background_image, image }) => (
-                        <div
-                            className={style.card}
-                            key={id}>
-                            <Card
-                                id={id}
-                                name={name}
-                                genres={genres}
-                                image={background_image || image}
-                            />
-                        </div>
+                {!allVideogamesArray.length ? (
+                    <div className={style.loading}>LOADING</div>
+                ) : (
+                    finalRender.map(
+                        ({ id, name, genres, background_image, image }) => (
+                            <div
+                                className={style.card}
+                                key={id}>
+                                <Card
+                                    id={id}
+                                    name={name}
+                                    genres={genres}
+                                    image={background_image || image}
+                                />
+                            </div>
+                        )
                     )
                 )}
+                ;
             </section>
 
             <section className={style.paginado}>
-                <button
-                    className={style.btn}
-                    onClick={handleFirst}
-                    disabled={page === 1}>
-                    ⇤
-                </button>
-                <button
-                    className={style.btn}
-                    onClick={handlePrev}
-                    disabled={page === 1}>
-                    ←
-                </button>
-                <button
-                    className={style.btn}
-                    onClick={handleNext}
-                    disabled={page === totalPages}>
-                    →
-                </button>
-                <button
-                    className={style.btn}
-                    onClick={handleLast}
-                    disabled={page === totalPages}>
-                    ⇥
-                </button>
+                {page !== 1 && (
+                    <button
+                        className={style.btn}
+                        onClick={handleFirst}>
+                        ⇤
+                    </button>
+                )}
+                {page !== 1 && page !== 2 && (
+                    <button
+                        className={style.btn}
+                        onClick={handlePrev}>
+                        ←
+                    </button>
+                )}
+                {page !== pages && page !== pages - 1 && (
+                    <button
+                        className={style.btn}
+                        onClick={handleNext}>
+                        →
+                    </button>
+                )}
+                {page !== pages && (
+                    <button
+                        className={style.btn}
+                        onClick={handleLast}>
+                        ⇥
+                    </button>
+                )}
             </section>
         </main>
     );
 };
-
 export default Cards;
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,14 +1,26 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
 import style from "./nav.module.css";
 import Search from "../search/Search";
-
+import { NavLink } from "react-router-dom";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateRenderArray } from "../../redux/actions";
 function Nav(props) {
+    const toRenderArray = useSelector((state) => state.toRenderArray);
+    const allVideogamesArray = useSelector((state) => state.allVideogamesArray);
+    const dispatch = useDispatch();
     return (
         <main className={style.nav}>
             <section className={style.searchcreate}>
-                <Search onSearch={props.onSearch} />
-
+                <div className={style.searchdiv}>
+                    <Search onSearch={props.onSearch} />
+                    <button
+                        className={style.button}
+                        onClick={() => {
+                            dispatch(updateRenderArray(allVideogamesArray));
+                        }}>
+                        Show all
+                    </button>
+                </div>
                 <button className={style.button}>
                     <NavLink
                         className={style.create}
@@ -19,33 +31,37 @@ function Nav(props) {
             </section>
 
             <section className={style.filters}>
-                <select
-                    className={style.desplegable}
-                    defaultValue="all"
-                    onChange={props.handleGenresChange}>
-                    <option value="all">Genres</option>
-                    <option value="Indie">Indie</option>
-                    <option value="Action">Action</option>
-                    <option value="Adventure">Adventure</option>
-                    <option value="Strategy">Strategy</option>
-                    <option value="RPG">RPG</option>
-                    <option value="Simulation">Simulation</option>
-                    <option value="Puzzle">Puzzle</option>
-                    <option value="Arcade">Arcade</option>
-                    <option value="Shooter">Shooter</option>
-                    <option value="Casual">Casual</option>
-                    <option value="Platformer">Platformer</option>
-                    <option value="Massively Multiplayer">
-                        Massively Multiplayer
-                    </option>
-                    <option value="Racing">Racing</option>
-                    <option value="Sports">Sports</option>
-                    <option value="Fighting">Fighting</option>
-                    <option value="Family">Family</option>
-                    <option value="Board Games">Board Games</option>
-                    <option value="Educational">Educational</option>
-                    <option value="Card">Card</option>
-                </select>
+                {toRenderArray.length < 20 ? (
+                    <></>
+                ) : (
+                    <select
+                        className={style.desplegable}
+                        defaultValue="all"
+                        onChange={props.handleGenresChange}>
+                        <option value="all">Genres</option>
+                        <option value="Indie">Indie</option>
+                        <option value="Action">Action</option>
+                        <option value="Adventure">Adventure</option>
+                        <option value="Strategy">Strategy</option>
+                        <option value="RPG">RPG</option>
+                        <option value="Simulation">Simulation</option>
+                        <option value="Puzzle">Puzzle</option>
+                        <option value="Arcade">Arcade</option>
+                        <option value="Shooter">Shooter</option>
+                        <option value="Casual">Casual</option>
+                        <option value="Platformer">Platformer</option>
+                        <option value="Massively Multiplayer">
+                            Massively Multiplayer
+                        </option>
+                        <option value="Racing">Racing</option>
+                        <option value="Sports">Sports</option>
+                        <option value="Fighting">Fighting</option>
+                        <option value="Family">Family</option>
+                        <option value="Board Games">Board Games</option>
+                        <option value="Educational">Educational</option>
+                        <option value="Card">Card</option>
+                    </select>
+                )}
 
                 <select
                     className={style.desplegable}
